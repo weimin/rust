67b9d8547d469c0bfa366732e31006d7fa680226
Wei Min Chan <weimin@weis-mbp.attlocal.net>
2021-11-29 07:14:33 -0800

    added hello cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/weimin/Documents/rust/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/weimin/Documents/rust/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/weimin/Documents/rust/hello_cargo/target/debug/hello_cargo: /Users/weimin/Documents/rust/hello_cargo/src/main.rs
